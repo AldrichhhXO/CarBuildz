b984105389071ebfa3ba474c1cee0d7afef95d05
AldrichhhXO <Dridgereb@yahoo.com>
2021-06-25 23:56:50 -0700

    Landing text

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -5,14 +5,54 @@ export default function Landing() {
     return (
         <div className = "Container">
             <Navbar />
-            <div className = "IntroductionContainer">
-                <h1>Welcome to Carbuilds.IO</h1>
+            <div>
+
+            </div>
+            <div className = "container" style = {{marginTop: '100px'}}>
+                <div className = "row row-cols-1 text-center">
+                    <div class="col">
+                        <div className = "row row-cols-1">
+                            <div className = "col">
+                                <h3>What is Carbuildz.IO?</h3>
+                            </div>
+                            <div className = "col">
+                                <p>
+                                    Carbuildz.IO is a web application focused on connecting automotive enthusiasts of various ages.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
             </div>
 
-            <div>
-                <p>CarBuildz.IO is used to promote and experiment with various builds for a specific car.</p>
-                <p>Be able to view and/or add cars into the system.</p>
-            </div>            
+            <div style = {{marginTop: '100px'}} className = "container">
+                <div className = "row row-cols-1 text-center">
+                    <div className = "col" style = {{marginBottom: '50px'}}>
+                        <h3>Carbuildz key Features</h3>
+                    </div>
+                    <div className = "row row-cols-3">
+                        <div className = "col">
+                            <p>
+                                Be able to search a vehicle within the database of various makes and models.
+                            </p>
+                        </div>
+                        <div className = "col">
+                            <p>
+                                Search builds based on the make, model, or even part
+                            </p>
+                        </div>
+                        <div className = "col">
+                            <p>
+                                Be able to upload your own build to share across the automotive community
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className = "container text-center" style = {{ marginTop: '50px'}}>
+                <button className = "btn btn-dark">View Builds</button>
+            </div>
         </div>
     )
 }
